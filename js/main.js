4080597e0c5ea0c519d39361c592f524905ed84d
IsaiasZc <coroxerox2@gmail.com>
2023-07-31 16:20:07 -0500

    last

--- js/main.js
+++ js/main.js
@@ -4,26 +4,28 @@ import { reset, calcularTip, calcularTotal, esCero, claseActivo } from './utilit
 /*------------- Elementos del DOM a usar --------------------*/
 
 // elemento <input> donde se registral el Bill o monto de la cuenta.
-const bill = document.querySelector("#input-bill");
+const billInput = document.querySelector("#input-bill");
 
 // en la seccion de "Select Tip %" hay 6 opciones para elegir el porcentaje de propina, aqui los seleccionamos a todos.
 const percentageButtons = [...document.querySelectorAll(".percentage-button")];
 
 // elemento <input> donde se indica el numero de personas.
-const inputPeople = document.querySelector("#input-people");
+const peopleInput = document.querySelector("#input-people");
 
 // En estos campos se muestra los totales
-const tipAmount = document.querySelector("#tip-amount");
-const total = document.querySelector("#total");
+const tipAmountInput = document.querySelector("#tip-amount");
+const totalInput = document.querySelector("#total");
 
-// Aqui se guarda el porcentaje del TIP
+// Aqui se guarda el porcentaje del TIP, bill y people
 let tip = 0;
+let bill = 0;
+let people = 0;
 
 
 /*--------------------------- FUNCIONES PARA LOS EVENTOS ----------------------------*/
 
 // Cada vez que el usuario escriba en el campo bill, ejecutará una tarea (función)
-bill.addEventListener("input", runProgram);
+billInput.addEventListener("input", runProgram);
 
 // Hay 6 opciones para propinas, así que la tarea debe ejecutarse por cada uno de ellos
 // este primer apartado se asegura de que cada uno realice esa tarea.
@@ -42,7 +44,7 @@ percentageButtons.forEach( button => {
 
 
 // Cada vez que el usuario seleccione o escriba una propina, ejecutará una tarea (función)
-inputPeople.addEventListener("input", runProgram)
+peopleInput.addEventListener("input", runProgram)
 
 
 /* Esta función se encarga de ejecutar todo el programa, ten en cuenta que tanto como para acceder
@@ -60,7 +62,8 @@ pero con la propiedad "textContent":
 
 */
 function runProgram(button) {
+  // Aqui capturamos los valores del usuario, y si alguno está vacio, lo dejamos en cero por defecto, para evitar errores en nuestro programa
   tip = parseFloat(button.value) || 0; // No Borrar
-
-  console.log(tip)
+  bill = parseFloat(billInput.value) || 0; // No Borrar
+  people = parseFloat(peopleInput.value) || 0; // No Borrar
 }
